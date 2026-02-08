b5bb25f4c98f03d87ea512efb64a85ef66cfbdac
Abhishek del Mundu <abhishek003mech@gmail.com>
2026-02-08 05:40:02 +0000

    Upload failed Firebase Storage: No default bucket found.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:658209518970:web:fc6bee4b77c26718ab92c3",
   "apiKey": "AIzaSyAu4k5Y3LakB0b9xgZRNPXfN17qGEbbty8",
   "authDomain": "studio-7977682669-c03b0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "658209518970"
+  "storageBucket": "studio-7977682669-c03b0.firebasestorage.app",
+  "messagingSenderId": "658209518970",
+  "measurementId": ""
 };
