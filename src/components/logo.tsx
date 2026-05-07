f7b985f3ef5e532e188dfd08335a5bd182fdc3ec
Abhishek del Mundu <abhishek003mech@gmail.com>
2026-05-07 01:28:50 +0000

    still in homepage in navbar logo is not showing

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
+// Importing the image directly since it's located in the same folder
+import logoImg from './logo.jpg';
 
 interface LogoProps {
   className?: string;
 }
 
 const Logo = ({ className }: LogoProps) => (
-  <div className={cn("relative h-10 w-40", className)}>
+  <div className={cn("relative h-10 w-40 flex items-center", className)}>
     <Image
-      src="/logo.jpg"
+      src={logoImg}
       alt="QuantisAI Logo"
-      fill
       className="object-contain"
       priority
       unoptimized
+      style={{ width: 'auto', height: '100%' }}
     />
   </div>
 );
